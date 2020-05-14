3d485fd9b9a282527d3b20e8cdac1a7d053b8114
leandro aprecido hermes <leandro.aparecido.hermes@gmail.com>
2020-05-14 15:07:47 -0300

    Logout set Playet Stop

--- src/screens/Logout/Logout.jsx
+++ src/screens/Logout/Logout.jsx
@@ -1,11 +1,26 @@
 import React from 'react'
 import { Redirect } from 'react-router-dom'
+import { connect } from 'react-redux'
 
+import * as actionsPlayer from '../../store/actions/player'
 import { deleteSessionToken } from '../../utils/utils'
 
-export default function Logout() {
+const Logout = function({ setPlayer }) {
+
+    setPlayer(false)
 
     deleteSessionToken()
 
     return <Redirect to="/login" />
 }
+
+const mapStateToProps = state => ({
+
+})
+
+const mapDispatchToProps = dispatch => ({
+    setPlayer: (playerData) => dispatch(actionsPlayer.status(playerData))
+})
+
+
+export default connect( mapStateToProps, mapDispatchToProps)(Logout)
